036ef26af5fa891dab9b8b6de7cc1afb34a0ee8d
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-01-20 11:54:41 +0100

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,33 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        // Instanziierung + Wertzuweisung
+        Cat cat = new Cat("Grizabella", "white", 29);
+
+        // Werte über Getter
+        output(cat.getStringAttributes("#name"));
+        output(cat.getStringAttributes("#color"));
+        output(cat.getAge());
+
+        output("-----------------------");
+
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
+
+        // Werte über Getter
+        output(cat2.getStringAttributes("#name"));
+        output(cat2.getStringAttributes("#color"));
+        output(cat2.getAge());
+
+    }
+
+  
+    public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
+
+
+}
+
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,35 @@
+package com.cc.java;
+
+public class Cat {
+
+    private String firstName;
+    private String furColor;
+    private int age;
+
+    public Cat(String firstName, String furColor, int age) {
+        this.firstName = firstName;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    /** Getter */
+    public String getStringAttributes(String op) {
+      switch (op) {
+        case "#name":
+            return firstName;
+        case "#color":
+            return furColor;
+        default:
+            return "ERROR";
+      }
+    }
+
+    public String getAge() {
+        return Integer.toString(age);
+    }
+
+    
+  
+
+
+}
